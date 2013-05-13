21fcbd65595e202b5ff130490e612248ed298c1d
unknown <lgure11@freeuni.edu.ge>
2013-05-13 23:25:46 +0400

    addes required jars

---
+++ src/ge/iauto/controller/ServletListener.java
@@ -0,0 +1,37 @@
+package ge.iauto.controller;
+
+import ge.iauto.server.PersistenceProvider;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ServletListener
+ *
+ */
+@WebListener
+public class ServletListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ServletListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0) {
+        
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0) {
+    //    PersistenceProvider.createEM().close();
+    }
+	
+}
